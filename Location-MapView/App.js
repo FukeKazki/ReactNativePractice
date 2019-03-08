78d5ee1607276983da5c923246ca5184e2bc24bc
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-03-08 23:27:25 +0900

    Create App.js
    🎉 :  initial commit

---
+++ Location-MapView/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import { Location, Permissions, MapView } from 'expo';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //初期化する
+      locationResult: null,
+      region: {
+        latitude: -3.7,
+        longitude: -38,
+        latitudeDelta: 0.2,
+        longitudeDelta: 0.2,
+      },
+    };
+  }
+  //コンポーネントがマウントされた後に実行
+  componentDidMount() {
+    //_getLocationAsync()を実行する
+    this._getLocationAsync();
+  }
+
+  _getLocationAsync = async () => {
+    //ユーザーに位置情報を取得して良いか確認した値
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    //ユーザーが拒否したとき
+    if (status !== 'granted') {
+      this.setState({
+        locationResult: 'Permission to access location was denied',
+      });
+    }
+    //位置情報を取得する getCurrentPositionAsync() 現在位置を一回だけ取得するメソッド
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+      locationResult: JSON.stringify(location),
+      region: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: this.state.region.latitude,
+          longitude: this.state.region.longitude,
+          latitudeDelta: this.state.region.latitudeDelta,
+          longitudeDelta: this.state.region.longitudeDelta,
+        }}>
+        <MapView.Marker
+          key={1}
+          coordinate={{
+            latitude: this.state.region.latitude,
+            longitude: this.state.region.longitude,
+          }}
+        />
+      </MapView>
+    );
+  }
+}
