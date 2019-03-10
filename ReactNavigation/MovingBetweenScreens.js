15eea6ba918765171f9e3fd6780e750ca7015084
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-03-10 15:12:29 +0900

    🎉 :  initial commit

---
+++ ReactNavigation/MovingBetweenScreens.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title = "Go to Details... again"
+          //もう一回移動? 
+          onPress = {() => this.props.navigation.push('Details')}
+        />
+        <Button
+          title = "Go to Home"
+          //移動する
+          onPress = {() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title = "Go back"
+          //戻るボタン
+          onPress = {() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
